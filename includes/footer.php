a7df95c87eb12de33766b25e654e2eae675c56ab
fateh05 <ft.fb05@gmail.com>
2020-09-17 04:38:52 +0200

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;AURES SHOP | All Rights Reserved | Contact Us: 0696398181</b></span></div>
+</footer>
